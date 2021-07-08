accafab680a5d92da28f34d1e1153ce7f590a5c0
TREEAID <78558662+TREEAID@users.noreply.github.com>
2021-07-08 15:26:11 +0000

    f

--- index.php
+++ index.php
@@ -88,74 +88,146 @@
   </div>
   <!-- Events-->
   <section id="events-cards">
-   <div class="row m-5">
-     <div class="col-lg-4 mb-5">
-     <div class="card">
-        <div class="card-header">
-          <img src="https://s.aolcdn.com/images/dims?client=fh7w6q744eiognjk&signature=d59d0cf6af1d779a3dca451e0ba259c33bbc6115&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fos%2Fab%2F_cms%2F2019%2F08%2F30142658%2F2020-jeep-wrangler-16.jpg&thumbnail=750%2C422&quality=80" alt="" />
-        </div>
-        <div class="card-body">
-          <span class="tag tag-teal">Technology</span>
-          <h4>Why is the Tesla Cybertruck designed the way it is?</h4>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-            tenetur distinctio neque?
-          </p>
-          <div class="user">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg" alt="" />
-            <div class="user-info">
-              <h5>Jerome Walton</h5>
-              <small>2h ago</small>
+    <div class="row m-5">
+      <div class="col-lg-4 mb-5">
+        <div class="card">
+          <div class="card-header">
+            <img src="https://s.aolcdn.com/images/dims?client=fh7w6q744eiognjk&signature=d59d0cf6af1d779a3dca451e0ba259c33bbc6115&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fos%2Fab%2F_cms%2F2019%2F08%2F30142658%2F2020-jeep-wrangler-16.jpg&thumbnail=750%2C422&quality=80" alt="" />
+          </div>
+          <div class="card-body">
+            <span class="tag tag-teal">NEWS</span>
+            <h4><a class="tag-link" href="#">isodec construct schools for communities...</a></h4>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+              tenetur distinctio neque?
+            </p>
+            <div class="user">
+              <!-- <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg" alt="" />-->
+              <div class="user-info">
+                <small>2h ago</small>
+              </div>
             </div>
           </div>
         </div>
       </div>
-     </div><!---->
-     <div class="col-lg-4 mb-5">
-     <div class="card">
-        <div class="card-header">
-          <img src="https://s.aolcdn.com/images/dims?client=fh7w6q744eiognjk&signature=d59d0cf6af1d779a3dca451e0ba259c33bbc6115&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fos%2Fab%2F_cms%2F2019%2F08%2F30142658%2F2020-jeep-wrangler-16.jpg&thumbnail=750%2C422&quality=80" alt="" />
-        </div>
-        <div class="card-body">
-          <span class="tag tag-teal">Technology</span>
-          <h4>Why is the Tesla Cybertruck designed the way it is?</h4>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-            tenetur distinctio neque?
-          </p>
-          <div class="user">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg" alt="" />
-            <div class="user-info">
-              <h5>Jerome Walton</h5>
-              <small>2h ago</small>
+      <!---->
+      <div class="col-lg-4 mb-5">
+        <div class="card">
+          <div class="card-header">
+            <img src="https://s.aolcdn.com/images/dims?client=fh7w6q744eiognjk&signature=d59d0cf6af1d779a3dca451e0ba259c33bbc6115&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fos%2Fab%2F_cms%2F2019%2F08%2F30142658%2F2020-jeep-wrangler-16.jpg&thumbnail=750%2C422&quality=80" alt="" />
+          </div>
+          <div class="card-body">
+            <span class="tag tag-teal">WEBINAR</span>
+            <h4>Webinar on</h4>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+              tenetur distinctio neque?
+            </p>
+            <div class="btn btn-primary">Register</div>
+            <div class="user">
+              <!--<img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg" alt="" />-->
+              <div class="user-info">
+                <small>2h ago</small>
+              </div>
             </div>
           </div>
         </div>
       </div>
-     </div><!---->
-     <div class="col-lg-4 mb-5">
-     <div class="card">
-        <div class="card-header">
-          <img src="https://s.aolcdn.com/images/dims?client=fh7w6q744eiognjk&signature=d59d0cf6af1d779a3dca451e0ba259c33bbc6115&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fos%2Fab%2F_cms%2F2019%2F08%2F30142658%2F2020-jeep-wrangler-16.jpg&thumbnail=750%2C422&quality=80" alt="" />
-        </div>
-        <div class="card-body">
-          <span class="tag tag-teal">Technology</span>
-          <h4>Why is the Tesla Cybertruck designed the way it is?</h4>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
-            tenetur distinctio neque?
-          </p>
-          <div class="user">
-            <img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg" alt="" />
-            <div class="user-info">
-              <h5>Jerome Walton</h5>
-              <small>2h ago</small>
+      <!---->
+      <div class="col-lg-4 mb-5">
+        <div class="card">
+          <div class="card-header">
+            <img src="https://s.aolcdn.com/images/dims?client=fh7w6q744eiognjk&signature=d59d0cf6af1d779a3dca451e0ba259c33bbc6115&image_uri=https%3A%2F%2Fs.aolcdn.com%2Fos%2Fab%2F_cms%2F2019%2F08%2F30142658%2F2020-jeep-wrangler-16.jpg&thumbnail=750%2C422&quality=80" alt="" />
+          </div>
+          <div class="card-body">
+            <span class="tag tag-teal">Blog</span>
+            <h4><a class="tag-link" href="#"> Why is the Tesla Cybertruck designed the way it is?</a></h4>
+            <p><a>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur
+                tenetur distinctio neque?
+              </a>
+            </p>
+            <div class="user">
+              <!--<img src="https://upload.wikimedia.org/wikipedia/commons/4/48/Outdoors-man-portrait_%28cropped%29.jpg" alt="" />-->
+              <div class="user-info">
+                <small>2h ago</small>
+              </div>
             </div>
           </div>
         </div>
       </div>
-     </div><!---->
-   </div>
+      <!---->
+    </div>
+  </section>
+  <section id="blogs_events">
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="container">
+          <div class="row" id="tab-menus">
+            <div class="col-12">
+              <h2 class="text-center mt-4 mb-5">BLOGS</h2>
+              <!-- Tabs navs -->
+              <ul class="nav nav-tabs" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" data-toggle="tab" href="#firstMenu"><i class="fab fa-html5"></i> LATEST BLOGS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#secondmenu"><i class="fab fa-wordpress"></i> ALL BLOGS</a>
+                </li>
+              </ul>
+              <!-- Tabs Content -->
+              <div class="tab-content">
+                <div id="firstMenu" class="tab-pane active">
+                  <h3 class="mt-4">What do you think about HTML5?</h3>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                </div>
+                <div id="secondmenu" class="tab-pane">
+                  <h3 class="mt-4">What do you think about WordPress?</h3>
+                  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ‘Content here, content here’, making it look like readable English.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-6">
+      <div class="container">
+          <div class="row" id="tab-menus">
+            <div class="col-12">
+              <h2 class="text-center mt-4 mb-5">EVENTS</h2>
+              <!-- Tabs navs -->
+              <ul class="nav nav-tabs" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" data-toggle="tab" href="#firstMenu"><i class="fa fa-html5"></i> ALL EVENTS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#secondmenu"><i class="fa fa-wordpress"></i> RECENTS EVENTS</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#thirdmenu"><i class="fa fa-bootstrap"></i> UPCOMING EVENTS</a>
+                </li>
+                
+              </ul>
+              <!-- Tabs Content -->
+              <div class="tab-content">
+                <div id="firstMenu" class="tab-pane active">
+                  <h3 class="mt-4">What do you think about HTML5?</h3>
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries</p>
+                </div>
+                <div id="secondmenu" class="tab-pane">
+                  <h3 class="mt-4">What do you think about WordPress?</h3>
+                  <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ‘Content here, content here’, making it look like readable English.</p>
+                </div>
+                <div id="thirdmenu" class="tab-pane">
+                  <h3 class="mt-4">What do you think about Bootstrap?</h3>
+                  <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </section>
 </main>
 <?php include_once('includes/footer2.php'); ?>
